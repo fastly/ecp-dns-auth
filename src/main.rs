bdb6b4e0b6f6ee1cf68d576ec47522a1542916c1
Mark Teodoro <mark@fastly.com>
2022-10-10 11:15:12 -0700

    break out lookup() function

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-use std::net::{IpAddr, Ipv4Addr};
-use std::{io, str::FromStr};
+use std::net::Ipv4Addr;
 
 use fastly::handle::client_ip_addr;
 use fastly::http::{header, Method, StatusCode};
@@ -7,10 +6,8 @@ use fastly::{Error, Request, Response};
 use serde_json::json;
 
 use trust_dns_proto::error::ProtoError;
-use trust_dns_proto::op::{Header, Message, ResponseCode};
-use trust_dns_proto::rr::{Name, RData, Record};
-use trust_dns_proto::serialize::binary::{BinDecodable, BinDecoder, BinEncoder};
-use trust_dns_server::authority::{MessageRequest, MessageResponseBuilder};
+use trust_dns_proto::op::{Header, Message, MessageType, OpCode, ResponseCode};
+use trust_dns_proto::rr::{Name, RData, Record, RecordType};
 
 fn debug_json() -> String {
     json!({
@@ -57,158 +54,70 @@ fn handle_doh_request(raw_msg: Vec<u8>) -> Result<Response, Error> {
     }
 }
 
-fn handle_request(raw_msg: Vec<u8>) -> Result<Vec<u8>, ProtoError> {
-    println!("raw_msg: {:X?}", raw_msg);
-
-    let mut decoder = BinDecoder::new(&raw_msg);
-    let msg_req = MessageRequest::read(&mut decoder)?;
-    println!("MessageRequest {:?}", msg_req);
-
-    // at this point we have a well-formed DNS query so even in the
-    // case of other errors we will be returning a DNS response.
-    let header = msg_req.header();
-    let query = msg_req.query();
-    println!("header: {:?}", header);
-    println!("query: {:?}", query);
-
-    // TODO actually look up the query
-    let answer = match client_ip_addr().unwrap() {
-        IpAddr::V4(ipv4) => Record::from_rdata(
-            Name::from_str("www.example.com").unwrap(),
-            5,
-            RData::A(ipv4),
-        ),
-        IpAddr::V6(ipv6) => Record::from_rdata(
-            Name::from_str("www.example.com").unwrap(),
-            5,
-            RData::AAAA(ipv6),
-        ),
-    };
-
-    let answer2 = Record::from_rdata(
-        Name::from_str("www.example.com").unwrap(),
-        5,
-        RData::A(Ipv4Addr::new(1, 2, 3, 4)),
-    );
-
-    let answer3 = Record::from_rdata(
-        Name::from_str("www.example.com").unwrap(),
-        5,
-        RData::A(Ipv4Addr::new(5, 6, 7, 8)),
-    );
-
-    let answers = [&answer, &answer2, &answer3];
-    let ns = None.into_iter();
-    let soa = None.into_iter();
-    let additionals = None.into_iter();
-
-    // build response
-    let mut response_header = Header::response_from_request(header);
-    response_header.set_authoritative(true);
-    let msg_resp = MessageResponseBuilder::from_message_request(&msg_req).build(
-        response_header,
-        answers,
-        ns,
-        soa,
-        additionals,
-    );
-    println!("msg_resp: {:?}", msg_resp);
-
-    let mut buffer = Vec::with_capacity(512);
-    let encode_result = {
-        let mut encoder = BinEncoder::new(&mut buffer);
-        msg_resp.destructive_emit(&mut encoder)
-    };
-
-    encode_result.map_err(|e| {
-        io::Error::new(
-            io::ErrorKind::Other,
-            format!("error encoding message: {}", e),
-        )
-    })?;
-
-    return Ok(buffer);
+fn dns_error(request: Message, rcode: ResponseCode) -> Result<Vec<u8>, ProtoError> {
+    Message::error_msg(request.id(), request.op_code(), ResponseCode::FormErr).to_vec()
 }
 
-fn handle_request_orig(raw_msg: Vec<u8>) -> Result<Vec<u8>, ProtoError> {
+struct LookupResult {
+    rcode: ResponseCode,
+    answers: Vec<Record>,
+    authority: Vec<Record>,
+    additionals: Vec<Record>,
+}
+
+fn lookup(name: Name, rr_type: RecordType) -> LookupResult {
+    let answer = Record::from_rdata(name, 5, RData::A(Ipv4Addr::new(93, 184, 216, 34)));
+    let answers = vec![answer];
+
+    let result = LookupResult {
+        rcode: ResponseCode::NoError,
+        answers: answers,
+        authority: Vec::new(),
+        additionals: Vec::new(),
+    };
+    return result;
+}
+
+fn handle_request(raw_msg: Vec<u8>) -> Result<Vec<u8>, ProtoError> {
     println!("raw_msg: {:X?}", raw_msg);
 
     let mut request = Message::from_vec(&raw_msg)?;
     println!("request: {:?}", request);
 
-    // at this point we have a well-formed DNS query so even in the
+    // at this point we have a well-formed DNS message so even in the
     // case of other errors we will be returning a DNS response.
 
-    let queries = request.take_queries();
-    let query = match queries.first() {
-        Some(query) if queries.len() == 1 => query,
-        _ => {
-            return Message::error_msg(request.id(), request.op_code(), ResponseCode::FormErr)
-                .to_vec()
-        }
-    };
-
-    println!("query: {:?}", query);
-
-    let mut response_header = Header::response_from_request(request.header());
-    response_header.set_authoritative(true);
-    // response_header.set_answer_count(1);
-    let mut response = Message::new();
-    response.set_header(response_header);
-
-    match client_ip_addr().unwrap() {
-        IpAddr::V4(ipv4) => {
-            response.add_answer(Record::from_rdata(
-                Name::from_str("www.example.com").unwrap(),
-                5,
-                RData::A(ipv4),
-            ));
-        }
-        IpAddr::V6(ipv6) => {
-            response.add_answer(Record::from_rdata(
-                Name::from_str("www.example.com").unwrap(),
-                5,
-                RData::AAAA(ipv6),
-            ));
-        }
+    // only handle queries
+    match (request.message_type(), request.op_code()) {
+        (MessageType::Query, OpCode::Query) => (),
+        _ => return dns_error(request, ResponseCode::NotImp),
     }
 
-    let answer2 = Record::from_rdata(
-        Name::from_str("www.example.com").unwrap(),
-        5,
-        RData::A(Ipv4Addr::new(1, 2, 3, 4)),
-    );
+    // make sure we have one and only one query
+    let query = match request.query() {
+        Some(query) if request.queries().len() == 1 => query,
+        _ => return dns_error(request, ResponseCode::FormErr),
+    };
+    println!("query: {:?}", query);
 
-    let answer3 = Record::from_rdata(
-        Name::from_str("www.example.com").unwrap(),
-        5,
-        RData::A(Ipv4Addr::new(5, 6, 7, 8)),
-    );
-    let answer4 = Record::from_rdata(
-        Name::from_str("www.example.com").unwrap(),
-        5,
-        RData::A(Ipv4Addr::new(5, 6, 7, 8)),
-    );
+    // build the base response
+    let mut response_header = Header::response_from_request(request.header());
+    response_header.set_authoritative(true);
+    let mut response = Message::new();
+    response.set_header(response_header);
+    response.add_query(query.clone());
 
-    response.add_answer(answer2);
-    response.add_answer(answer3);
-    response.add_answer(answer4);
+    // do the lookup
+    let result = lookup(query.name().to_lowercase(), query.query_type());
 
-    response.add_queries(queries);
-
+    // update and send the response
+    response.set_response_code(result.rcode);
+    response.insert_answers(result.answers);
+    response.insert_name_servers(result.authority);
+    response.insert_additionals(result.additionals);
     println!("response: {:?}", response);
 
     return response.to_vec();
-
-    /*
-        return Ok(vec![
-            0x00, 0x00, 0x81, 0x80, 0x00, 0x01, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x03, 0x77, 0x77,
-            0x77, 0x07, 0x65, 0x78, 0x61, 0x6D, 0x70, 0x6C, 0x65, 0x03, 0x63, 0x6F, 0x6D, 0x00, 0x00,
-            0x1C, 0x00, 0x01, 0xC0, 0x0C, 0x00, 0x1C, 0x00, 0x01, 0x00, 0x00, 0x0E, 0x7D, 0x00, 0x10,
-            0x20, 0x01, 0x0D, 0xB8, 0xAB, 0xCD, 0x00, 0x12, 0x00, 0x01, 0x00, 0x02, 0x00, 0x03, 0x00,
-            0x04,
-        ]);
-    */
 }
 
 #[fastly::main]
