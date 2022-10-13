2b6748300cf550cbcccece17ec320092b8b1f529
Mark Teodoro <mark@fastly.com>
2022-10-12 17:58:44 -0700

    lookup logic and support for A and SOA rrtypes

--- src/lookup.rs
+++ src/lookup.rs
@@ -1,8 +1,11 @@
 use std::net::Ipv4Addr;
+use std::str::FromStr;
 
-// use fastly::handle::client_ip_addr;
+use fastly::config_store::ConfigStore;
+use serde::Deserialize;
 
 use trust_dns_proto::op::ResponseCode;
+use trust_dns_proto::rr::rdata;
 use trust_dns_proto::rr::{Name, RData, Record, RecordType};
 
 pub struct LookupResult {
@@ -14,14 +17,149 @@ pub struct LookupResult {
 
 pub fn lookup(name: &Name, rr_type: RecordType) -> LookupResult {
     println!("lookup {}:{}", name, rr_type);
-    let name = name.to_lowercase();
-    let answer = Record::from_rdata(name, 5, RData::A(Ipv4Addr::new(93, 184, 216, 34)));
-    let answers = vec![answer];
 
-    LookupResult {
+    let mut result = LookupResult {
         rcode: ResponseCode::NoError,
-        answers: answers,
+        answers: Vec::new(),
         authority: Vec::new(),
         additionals: Vec::new(),
+    };
+
+    let store = ConfigStore::open("zones");
+
+    let mut lname = name.to_lowercase();
+    lname.set_fqdn(true);
+
+    // look for the name or the wildcard
+    match store
+        .get(&lname.to_string())
+        .or_else(|| store.get(&lname.clone().into_wildcard().to_string()))
+    {
+        Some(rrmapstr) => {
+            // found something; decode the rr map
+            let rrmap: JsonRRMap = match serde_json::from_str(&rrmapstr) {
+                Ok(rrmap) => rrmap,
+                Err(err) => {
+                    println!("ERROR: bad json data in {} or wildcard: {}", name, err);
+                    result.rcode = ResponseCode::ServFail;
+                    return result;
+                }
+            };
+            println!("{}: {:?}", lname, rrmap);
+            result.answers = match (rr_type, rrmap) {
+                (RecordType::A, JsonRRMap { A: Some(A), .. }) => json_a(name, A),
+                _ => Vec::new(), // couldn't find rrs of the requested type
+            }
+        }
+        _ => {
+            // name and wildcard don't exist
+            result.rcode = ResponseCode::NXDomain;
+        }
     }
+
+    if result.answers.len() >= 1 {
+        return result;
+    }
+
+    // If we got here we failed to return answers because the name
+    // and wildcard don't exist or don't have records of rr_type.
+    // Walk up the tree looking for the SOA record at the top of the zone.
+    // If we find it, add it to the authority section and return.
+    // Otherwise return REFUSED because we don't serve this domain.
+
+    lname = lname.base_name();
+    while lname.num_labels() >= 2 {
+        println!("looking for {}", lname);
+        match store.get(&lname.to_string()) {
+            Some(rrmapstr) => {
+                // found a name; decode the rr map
+                let rrmap: JsonRRMap = match serde_json::from_str(&rrmapstr) {
+                    Ok(rrmap) => rrmap,
+                    Err(err) => {
+                        println!("ERROR: bad json data in {}: {}", name, err);
+                        result.rcode = ResponseCode::ServFail;
+                        return result;
+                    }
+                };
+                println!("{}: {:?}", lname, rrmap);
+                match rrmap.SOA {
+                    Some(rrs) => {
+                        result.authority = json_soa(name, rrs);
+                        return result;
+                    }
+                    _ => {
+                        // name exists, but no SOA record
+                        lname = lname.base_name();
+                    }
+                }
+            }
+            _ => {
+                // name doesn't exist
+                lname = lname.base_name();
+            }
+        }
+    }
+
+    result.rcode = ResponseCode::Refused;
+    result
+}
+
+#[derive(Deserialize, Debug)]
+struct JsonRRMap {
+    A: Option<JsonA>,
+    SOA: Option<JsonSOA>,
+}
+
+#[derive(Deserialize, Debug)]
+struct JsonA {
+    ttl: u32,
+    values: Vec<String>,
+}
+
+#[derive(Deserialize, Debug)]
+struct JsonSOAValue {
+    mname: String,
+    rname: String,
+    serial: u32,
+    refresh: i32,
+    retry: i32,
+    expire: i32,
+    minimum: u32,
+}
+
+#[derive(Deserialize, Debug)]
+struct JsonSOA {
+    ttl: u32,
+    value: JsonSOAValue,
+}
+
+fn json_soa(name: &Name, rrs: JsonSOA) -> Vec<Record> {
+    vec![Record::from_rdata(
+        name.clone(),
+        rrs.ttl,
+        RData::SOA(rdata::SOA::new(
+            // TODO handle errors here
+            Name::from_str(&rrs.value.mname).unwrap(),
+            Name::from_str(&rrs.value.rname).unwrap(),
+            rrs.value.serial,
+            rrs.value.refresh,
+            rrs.value.retry,
+            rrs.value.expire,
+            rrs.value.minimum,
+        )),
+    )]
+}
+
+fn json_a(name: &Name, rrs: JsonA) -> Vec<Record> {
+    rrs.values
+        .iter()
+        .map(|v| {
+            Record::from_rdata(
+                name.clone(),
+                rrs.ttl,
+                // TODO handle errors here
+                RData::A(Ipv4Addr::from_str(v).unwrap()),
+            )
+        })
+        .collect()
 }
